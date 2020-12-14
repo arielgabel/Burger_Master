6093d25c89a80735841b19a7a2d538a3b7bdeb2e
arielgabel <arielgabel11@gmail.com>
2020-12-14 16:52:32 +0200

    adding fillesfdff

---
+++ Assets/Code/insideContainer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class insideContainer : MonoBehaviour
+{
+    public bool isCollideWithPlayer = false;
+    public Transform myFood;
+    public GameObject myPlayer;
+    public joyButton m_getInfo; // need to check how
+
+    public Transform getFoodObject()
+    {
+        return myFood;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCollideWithPlayer && m_getInfo.ReturnIfPressed()) // if theres a collision and the pickup button is pressed
+        {
+            myFood.transform.SetParent(myPlayer.transform);
+           // Vector3 playerLoc = myPlayer.transform.localPosition;
+            
+            //myFood.localPosition = new Vector3(playerLoc.x + 10, playerLoc.y + 45, playerLoc.z + 30);
+        }
+        else
+        {
+            //maybe need maybe not. probabely need to put back the item
+            // forDebug = true;
+        }
+    }
+
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isCollideWithPlayer = true;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.HSVToRGB(0.077f, 0.489f, 1.0f);
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isCollideWithPlayer = false;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.HSVToRGB(0.077f, 0.489f, 0.717f);
+        }
+    }
+
+
+}
